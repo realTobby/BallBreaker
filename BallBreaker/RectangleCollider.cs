8a4e4b7c38700f1cba24417bac4e38a013db0035
Tobias Kattanek <tkattanek00@gmail.com>
2021-10-06 15:50:26 +0200

    Collision tweaks

--- BallBreaker/RectangleCollider.cs
+++ BallBreaker/RectangleCollider.cs
@@ -19,27 +19,31 @@ namespace BallBreaker
 
         public CollisionDirection IsColliding(int colliderX, int colliderY, int targetX, int targetY)
         {
-            if (targetX > colliderX && targetX < colliderX + ColliderWidth
-                && targetY > colliderY && targetY < colliderY + ColliderHeight)
+            if (targetX >= colliderX && targetX <= colliderX + ColliderWidth
+                && targetY >= colliderY && targetY <= colliderY + ColliderHeight)
             {
 
-                if(targetX > colliderX && targetX >= colliderX+ColliderWidth)
+                if(targetX >= colliderX && targetX >= colliderX+ColliderWidth)
                 {
+                    System.Console.WriteLine("Collide Right!");
                     return CollisionDirection.Right;
                 }
 
                 if(targetX <= colliderX)
                 {
+                    System.Console.WriteLine("Collide Left!");
                     return CollisionDirection.Left;
                 }
 
                 if(targetY > colliderY && targetY >= colliderY+ColliderHeight)
                 {
+                    System.Console.WriteLine("Collide Down!");
                     return CollisionDirection.Down;
                 }
 
                 if(targetY <= colliderY)
                 {
+                    System.Console.WriteLine("Collide Right!");
                     return CollisionDirection.Top;
                 }
 
